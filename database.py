9de9fcde329780e2d0563aea46a8a237e55b08b4
OxanaKon <121063654+OxanaKon@users.noreply.github.com>
2023-03-27 00:55:10 +0300

    Update database.py

--- database.py
+++ database.py
@@ -10,38 +10,35 @@ def create_db():
     sqlCreateDatabase = "CREATE DATABASE database_db;"
     cursor.execute(sqlCreateDatabase);
     con.close()
-
-
-
+    
 def create_table_seen_users(connection):
-    # создаем таблицу "Просмотренные пользователи"
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS seen_users(
-            id serial,
-            vk_id varchar(50) PRIMARY KEY);"""
-        )
-        connection.commit()
-    print("[INFO] Table SEEN_USERS was created.")
+# создаем таблицу "Просмотренные пользователи"
+with connection.cursor() as cursor:
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS seen_users(
+        id serial,
+        vk_id varchar(50) PRIMARY KEY);"""
+    )
+    connection.commit()
+print("[INFO] Table SEEN_USERS was created.")
 
-
-def insert_data_seen_users(connection, vk_id):
-   # вставляем данные в таблицу
+def insert_data_seen_users(vk_id, connection):
+    # вставляем данные в таблицу
     with connection.cursor() as cursor:
         cursor.execute(
             f"""INSERT INTO seen_users (vk_id)
             VALUES ('{vk_id}')
-            OFFSET '{offset}';"""
+            """
         )
 
-def check_user(connection, vk_id):
+ def check_user( vk_id, connection):
     with connection.cursor() as cursor:
         cursor.execute(
                 f"""SELECT EXISTS(
                 SELECT * FROM seen_users WHERE id = '{vk_id}');"""
             )
         connection.commit()
-        cur.fetchone() is None
+        cursor.fetchone() is None
         True
 
 
