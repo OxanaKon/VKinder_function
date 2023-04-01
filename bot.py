a72ab60a9b696058c3bafbe896f53e18dc8c1701
OxanaKon <121063654+OxanaKon@users.noreply.github.com>
2023-04-02 00:12:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,31 +3,40 @@ from random import randrange
 import vk_api
 import datetime
 from vk_api.longpoll import VkLongPoll, VkEventType
-from token_2 import token_group , access_token, V
-from database import insert_data_seen_users, check_user, create_db, create_table_seen_users
+from token_2 import token_group, access_token, V
+from database import insert_data_seen_users, check_user, create_table_seen_users
+from vk_api.exceptions import ApiError
 
-vk_session = vk_api.VkApi(token = token_group)
+vk_session = vk_api.VkApi(token=token_group)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
+
+ALIASE = {'city': 'город',
+          'sex': 'пол партнёра',
+          'bdate': 'свой возраст'
+          }
+
 
 def write_msg(user_id, message):
     vk_session.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7), })
 
+
 def paste_foto(user_id, attachment):
     vk_session.method('messages.send', {'user_id':user_id, 'attachment':attachment, 'random_id':randrange(10 ** 7),})
+
 
 def search_users(age_from, age_to, sex, city, status):
     all_persons = []
     link_profile = 'https://vk.com/id'
     vk = vk_api.VkApi(token=access_token)
     response = vk.method('users.search',
-                          {'age_from': age_from,
-                           'age_to': age_to,
+                         {'age_from': age_from,
+                          'age_to': age_to,
                           'sex': sex,
                           'hometown': city,
                           'status': status,
                           'count': 100,
-                           })
+                          })
     try:
         response = response['items']
     except KeyError:
@@ -39,26 +48,28 @@ def search_users(age_from, age_to, sex, city, status):
                                 'last_name': element['last_name'],
                                 'id': element['id']
                                 }
-                            )
+                               )
     return all_persons
 
- # Ищем фото
+
+# Ищем фото
 
 def get_photos(owner_id):
     vk = vk_api.VkApi(token=access_token)
 
-    params = {    'access_token': access_token,
-                  'v': V,
-                  'owner_id': owner_id,
-                  'album_id': 'profile',
-                  'count': 10,
-                  'extended': 1}
+    params = {'access_token': access_token,
+              'v': V,
+              'owner_id': owner_id,
+              'album_id': 'profile',
+              'count': 10,
+              'extended': 1}
     result = vk.method('photos.get', params)
 
     photos = [(item['likes']['count'], f"photo{item['owner_id']}_{item['id']}")
-              for item in result ['items']]
+              for item in result['items']]
     photos = sorted(photos)
     photos = [item[1] for item in photos][:3]
+
     return photos
 
 
@@ -70,32 +81,62 @@ def get_profile_info(user_id):
                            'fields': 'bdate,sex,city'
                            }
                           )
+        print(info)
     except ApiError:
         return None
 
-    year = datetime.datetime.now().year
-    
-    if 'bdate' not in info:
-       write_msg(user_id, 'Введите Ваш возраст')
+    store = ['city', 'sex', 'bdate']
+    out = {}
+    for fields, value in info.items():
+        if fields in store:
+            if fields == 'sex':
+                out[fields] = 1 if value == 2 else 2
+                store.remove('sex')
+            if fields == 'bdate':
+                year = datetime.datetime.now().year
+                b_year = int(info['bdate'].split('.')[2])
+                age_from = year - b_year - 5
+                age_to = year - b_year + 5
+                out['age_from'] = age_from
+                out['age_to'] = age_to
+                store.remove('bdate')
+            if fields == 'city':
+                out['city'] = value['title']
+                store.remove('city')
 
-    else:
-        b_year = int(info['bdate'].split('.')[2])
-        age_from = year - b_year - 5
-        age_to = year - b_year + 5
+    return out, store
 
-    if 'city' in info:
-        city = info['city']['title']
+
+def get_city(input):
+    if input.isdigit():
+        return None
     else:
-        write_msg(event.user_id, 'введите город')
+        return input.strip()
+
+    pass
+
+
+def get_sex(input):
+    sex = 1
+    if input.isdigit():
+        return None
+    else:
+        if input[0].lower() == 'ж':
+            sex = 2
+    return sex
+
+
+def get_bdate(input):
+    if input.isdigit():
+        return (int(input) - 5, int(input) + 5)
+    else:
         return None
 
-    return {'id': info['id'],
-            'city': city,
-            'age_from': age_from,
-            'age_to': age_to,
-            'sex': info['sex']
-            }
 
+SWITH = {'city': get_city,
+         'sex': get_sex,
+         'bdate': get_bdate
+         }
 
 if __name__ == '__main__':
     connections = psycopg2.connect(database='database_db', user='postgres', password='lkjh9874')
@@ -114,13 +155,37 @@ if __name__ == '__main__':
 
                     write_msg(event.user_id, 'Начинаю поиск...')
 
-                    info = get_profile_info(event.user_id)
-                  
+                    info, fall_filds = get_profile_info(event.user_id)
+                    print(info)
+                    print(fall_filds)
+
+                    if fall_filds:
+
+                        for filds in fall_filds:
+                            good_value = None
+                            while good_value is None:
+                                write_msg(event.user_id,
+                                          f'В вашем профиле недостаточно данных, введите {ALIASE[filds]}')
+                                longpoll.update_longpoll_server()
+                                for fall_event in longpoll.listen():
+                                    if fall_event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                                        if fall_event.user_id == vk_id:
+                                            fall_value = fall_event.text
+                                            good_value = SWITH[filds](fall_value)
+                                            break
+                            if filds == 'bdate':
+                                info['age_from'] = good_value[0]
+                                info['age_to'] = good_value[1]
+                            else:
+                                info[filds] = good_value
+
+                    print(info)
+
                     result = search_users(info['age_from'],
-                                      info['age_to'],
-                                      1 if info['sex'] == 2 else 2,
-                                      info['city'],
-                                      6)
+                                          info['age_to'],
+                                          info['sex'],
+                                          info['city'],
+                                          6)
 
                     if result:
                         user = result.pop()
