b6a03db9a858c1d82066a41e9f7cd280ebfd0c7f
OxanaKon <121063654+OxanaKon@users.noreply.github.com>
2023-04-08 23:36:30 +0300

    Update database.py

--- database.py
+++ database.py
@@ -23,14 +23,6 @@ def create_table_seen_users(connection):
         connection.commit()
     print("[INFO] Table SEEN_USERS was created.")
 
-def insert_data_seen_users(vk_id, connection):
-    # вставляем данные в таблицу
-    with connection.cursor() as cursor:
-        cursor.execute(
-            f"""INSERT INTO seen_users (vk_id)
-            VALUES ('{vk_id}')
-            """
-        )
 
 def check_user( vk_id, connection):
     with connection.cursor() as cursor:
@@ -42,12 +34,27 @@ def check_user( vk_id, connection):
         cursor.fetchone() is None
         True
 
+        
+def insert_data_seen_users(vk_id, connection):
+    # вставляем данные в таблицу
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute(
+                f"""INSERT INTO seen_users (vk_id)
+                VALUES ('{vk_id}')
+                """
+            )
+
+        except psycopg2.errors.UniqueViolation:
+           pass
+
+
+
 if __name__ == '__main__':
     create_db()
     connections = psycopg2.connect(database='database_db', user='postgres', password='lkjh9874')
     create_table_seen_users(connections)
     check_user(vk_id, connection)
     connections.close()
-
 
 
